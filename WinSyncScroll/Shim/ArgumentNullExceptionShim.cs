d6d1c13bf60fa47afa5828456586644e5a7e764b
Ilia Burakov <burakov.ilia@gmail.com>
2024-07-26 07:16:34 -0400

    fix

--- WinSyncScroll/Shim/ArgumentNullExceptionShim.cs
+++ WinSyncScroll/Shim/ArgumentNullExceptionShim.cs
@@ -19,7 +19,6 @@ public static class ArgumentNullExceptionShim
     /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
     /// <param name="argument">The pointer argument to validate as non-null.</param>
     /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
-    [CLSCompliant(false)]
     public static unsafe void ThrowIfNull([NotNull] void* argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
     {
         if (argument is null)
