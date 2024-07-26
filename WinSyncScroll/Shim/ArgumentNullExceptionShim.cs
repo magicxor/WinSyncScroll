fe1d22c56ad6cafc1482cfbe1e37fd5065414816
Ilia Burakov <burakov.ilia@gmail.com>
2024-07-26 07:12:48 -0400

    add shim

---
+++ WinSyncScroll/Shim/ArgumentNullExceptionShim.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace WinSyncScroll.Shim;
+
+public static class ArgumentNullExceptionShim
+{
+    /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
+    /// <param name="argument">The reference type argument to validate as non-null.</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+    public static void ThrowIfNull([NotNull] object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        if (argument is null)
+        {
+            Throw(paramName);
+        }
+    }
+
+    /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
+    /// <param name="argument">The pointer argument to validate as non-null.</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+    [CLSCompliant(false)]
+    public static unsafe void ThrowIfNull([NotNull] void* argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        if (argument is null)
+        {
+            Throw(paramName);
+        }
+    }
+
+    /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
+    /// <param name="argument">The pointer argument to validate as non-null.</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+    internal static unsafe void ThrowIfNull(IntPtr argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        if (argument == IntPtr.Zero)
+        {
+            Throw(paramName);
+        }
+    }
+
+    [DoesNotReturn]
+    private static void Throw(string? paramName) =>
+        throw new ArgumentNullException(paramName);
+}
