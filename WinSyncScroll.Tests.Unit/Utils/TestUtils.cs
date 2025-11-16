a072d38248849fa621ab5c74d27d386f7354fbbe
Ilia Burakov <burakov.ilia@gmail.com>
2025-11-16 04:42:10 -0500

    move CreateLParam methods to Common lib

--- WinSyncScroll.Tests.Unit/Utils/TestUtils.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WinSyncScroll.Tests.Unit.Utils;
-
-public static class TestUtils
-{
-    public static IntPtr CreateLParam(ushort hiWord, ushort loWord)
-    {
-        return (IntPtr)((hiWord << 16) | (loWord & 0xFFFF));
-    }
-}
